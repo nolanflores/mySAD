9d9a0cc2500576b5f1696d1a7502e713d9a2f9bd
nolanflores <78749402+nolanflores@users.noreply.github.com>
2022-05-26 21:36:21 -0700

    small additions to shop

--- src/com/core/Store.java
+++ src/com/core/Store.java
@@ -1,5 +1,29 @@
 package com.core;
 
+import java.awt.*;
+
 public class Store {
     private Pet[] merch;
+    private int round;
+
+    public Store(int r){
+        round = r;
+        merch = new Pet[3+(r-1)/3];
+    }
+
+    public void roll(){
+        for(Pet p : merch){
+
+        }
+    }
+
+    public void drawInShop(Graphics2D g, double xscale, double yscale){
+        for(int i = 0; i < merch.length; i++){
+            Pet p = merch[i];
+            if(p != null){
+                p.setLoc(.14+(.075*i), .4);
+                p.draw(g,xscale*.9,yscale*.9);
+            }
+        }
+    }
 }
